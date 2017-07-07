4115fef04bde7d4b6eb347dd11afb3bcddea8dbe
Preetam Sarkar <PSarkar@jjkeller.com>
2017-07-07 15:20:19 +0530

    Added files

---
+++ PreRequisites.sh
@@ -0,0 +1,4 @@
+sudo apt install python
+sudo apt install python-pip
+sudo pip install --user boto
+sudo apt install ansible
---
+++ SREXrcise/PreRequisites.sh
@@ -0,0 +1,5 @@
+sudo apt install python
+sudo apt install python-pip
+pip install boto
+git config --global user.email "newpreetam@gmail.com"
+git config --global user.name "newpreetam"
